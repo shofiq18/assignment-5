831e2cb924681ee7dc7a4ba05e2af97baf6ab2dd
shofiq18 <shafiqulislam25021998@gmail.com>
2024-09-24 01:15:24 +0600

    added toggle button

---
+++ button.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const donationBtn = document.getElementById('donation-btn');
+    const historyBtn = document.getElementById('history-btn');
+
+
+    // Donation button
+
+    donationBtn.addEventListener('click', function(){
+        donationBtn.classList.toggle('#B4F461');
+        donationBtn.classList.toggle('bg-slate-50');
+        historyBtn.classList.remove('bg-green-500');
+        historyBtn.classList.add('bg-slate-50');
+    });
+
+
+    // History button
+
+    historyBtn.addEventListener('click', function(){
+        historyBtn.classList.toggle('bg-green-500');
+        historyBtn.classList.toggle('bg-slate-50');
+        donationBtn.classList.remove('bg-green-500');
+        donationBtn.classList.add('bg-slate-50');
+    });
+});
